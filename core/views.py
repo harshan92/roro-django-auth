7fb8b8e5b102e224a0a949a6602cc6f6cc973390
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2023-02-18 13:46:59 +0530

    implemented register endpoint

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
 class RegisterAPIView(APIView):
-    
+    def post(self, request):
+        return Response(request.data)
