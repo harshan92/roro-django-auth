3c3d88452abb7c0f17cebb459cacfb7c72a31a7c
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2023-02-18 14:08:11 +0530

    implemented serializer for user

--- core/views.py
+++ core/views.py
@@ -1,5 +1,16 @@
 from rest_framework.views import APIView
+from rest_framework import exceptions
 from rest_framework.response import Response
+from .serializer import UserSerializer
 class RegisterAPIView(APIView):
     def post(self, request):
-        return Response(request.data)
+        data=request.data
+
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException("Password doesn't match!")
+        
+        serializer=UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
