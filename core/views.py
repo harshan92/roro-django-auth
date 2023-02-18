642dffd27813c7147e3370a76f584055a8e77855
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2023-02-18 20:30:37 +0530

    implemented authenticate middleware

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from rest_framework.authentication import get_authorization_header
 from rest_framework.response import Response
 from .serializer import UserSerializer
 from .models import User
-from .authentication import create_access_token, create_refresh_token, decode_access_token
+from .authentication import JWTAuthentication, create_access_token, create_refresh_token, decode_access_token
 
 class RegisterAPIView(APIView):
     def post(self, request):
@@ -44,15 +44,6 @@ class LoginAPIView(APIView):
         return response
 
 class UserAPIView(APIView):
+    authentication_classes=[JWTAuthentication]
     def get(self, request):
-        auth = get_authorization_header(request).split()
-
-        if auth and len(auth)==2:
-            token=auth[1].decode('utf-8')
-            id=decode_access_token(token)
-            user=User.objects.get(pk=id)
-            if user:
-                serializer=UserSerializer(user)
-                return Response(serializer.data)
-        
-        raise exceptions.AuthenticationFailed('unauthenticated')
+       return Response(UserSerializer(request.user).data)
