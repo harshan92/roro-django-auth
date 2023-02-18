709960eae9930e53bd5ea83ff622deaae1c6e0b5
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2023-02-18 19:36:15 +0530

    implemented authenticated user endpoint

--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework import exceptions
+from rest_framework.authentication import get_authorization_header
 from rest_framework.response import Response
 from .serializer import UserSerializer
 from .models import User
-from .authentication import create_access_token, create_refresh_token
+from .authentication import create_access_token, create_refresh_token, decode_access_token
 
 class RegisterAPIView(APIView):
     def post(self, request):
@@ -41,3 +42,17 @@ class LoginAPIView(APIView):
         }
 
         return response
+
+class UserAPIView(APIView):
+    def get(self, request):
+        auth = get_authorization_header(request).split()
+
+        if auth and len(auth)==2:
+            token=auth[1].decode('utf-8')
+            id=decode_access_token(token)
+            user=User.objects.get(pk=id)
+            if user:
+                serializer=UserSerializer(user)
+                return Response(serializer.data)
+        
+        raise exceptions.AuthenticationFailed('unauthenticated')
