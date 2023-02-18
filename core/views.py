29afd5d22a111e949268daf56258edc7fbc4f758
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2023-02-18 18:20:30 +0530

    Implemented login endpoint

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework import exceptions
 from rest_framework.response import Response
 from .serializer import UserSerializer
+from .models import User
+
 class RegisterAPIView(APIView):
     def post(self, request):
         data=request.data
@@ -12,5 +14,20 @@ class RegisterAPIView(APIView):
         serializer=UserSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+
+        return Response(serializer.data)
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email=request.data['email']
+        password=request.data['password']
+
+        user=User.objects.filter(email=email).first()
+        if user is None:
+            raise exceptions.AuthenticationFailed('Invalid credentials')
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('Invalid credentials')
+
+        serializer=UserSerializer(user)
 
         return Response(serializer.data)
